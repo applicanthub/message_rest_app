817789a02fbfca9e1591cd4b81a43eb9419eae72
applicanthub <47771075+applicanthub@users.noreply.github.com>
2019-02-21 15:12:33 -0500

    Update HealthEndpointsAlgebra.scala

--- app_server/src/main/scala/com/spyops/infrastructure/endpoints/finch/app/HealthEndpointsAlgebra.scala
+++ app_server/src/main/scala/com/spyops/infrastructure/endpoints/finch/app/HealthEndpointsAlgebra.scala
@@ -3,7 +3,12 @@ package com.spyops.infrastructure.endpoints.finch.app
 import com.spyops.infrastructure.endpoints.finch.FinchIOEndpoint
 
 trait HealthEndpointsAlgebra {
-
+  
+  /**
+    * Health-checker.
+    *
+    * @author Nick Odumo Feb 2019
+    */
   def healthCheckString: FinchIOEndpoint[String]
 
 }
