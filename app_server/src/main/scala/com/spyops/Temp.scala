1e3b42b5a1f4a40096bc1e4a3e16b40d3b552475
applicanthub <47771075+applicanthub@users.noreply.github.com>
2019-02-23 11:02:45 -0500

    Update Temp.scala

--- app_server/src/main/scala/com/spyops/Temp.scala
+++ app_server/src/main/scala/com/spyops/Temp.scala
@@ -14,24 +14,18 @@ object Temp {
   type _readerInt[R] = ReaderInt |= R
   type _writerString[R] = WriterString |= R
 
-  def program[R: _readerInt: _writerString: _eval]: Eff[R, Int] = for {
-    // get the configuration
+  def program[R: _readerInt: _writerString: _eval]: Eff[R, Int] = for { 
     n <- ask[R, Int]
-
-    // log the current configuration value
+ 
     _ <- tell("the required power is " + n)
 
-    // compute the nth power of 2
     a <- delay(math.pow(10, n.toDouble).toInt)
 
     _ <- tell("Logging some text.")
 
-    // log the result
     _ <- tell("the result is " + a)
   } yield a
 
-  // run the action with all the interpreters
-  // each interpreter running one effect
   def testIt(int: Int) = program[Stack].runReader(int).runWriter.runEval.run
 
   def logIt(): Unit = println(testIt(100))
