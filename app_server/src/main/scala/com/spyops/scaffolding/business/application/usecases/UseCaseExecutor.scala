b61f98b8b0bcf6841dbdbf02d4ed5192e9c7d769
Messgenger <qlikstuff@mail.com>
2019-02-23 23:25:36 -0500

    Cleaned up code

---
+++ app_server/src/main/scala/com/spyops/scaffolding/business/application/usecases/UseCaseExecutor.scala
@@ -0,0 +1,27 @@
+package com.spyops.scaffolding.business.application.usecases
+
+abstract class UseCaseExecutor[F[_], Input, Output] {
+
+  /**
+   * Log the business logic result.
+   *
+   * @author Nick Odumo Feb 2019
+   * @param value Loggable value
+   */
+  def logWriterResult(value: String): F[Unit]
+
+  /**
+   * Executes the business logic of your application's use case.
+   *
+   * @author Nick Odumo Feb 2019
+   * @param useCase UseCase object
+   * @param request An object containing request data in public fields
+   * @return object An object containing response data in public fields
+   */
+  def executeUseCase(useCase: Nothing, request: Input): F[Output]
+
+}
+
+object UseCaseExecutor {
+
+}
