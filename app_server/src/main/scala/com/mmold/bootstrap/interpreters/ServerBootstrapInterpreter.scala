bef636390779009d3bff41f28aa00f10354e59de
Nick Odumo <nodumo@kivuto.com>
2019-02-25 15:00:15 -0500

    Comment added

--- app_server/src/main/scala/com/mmold/bootstrap/interpreters/ServerBootstrapInterpreter.scala
+++ app_server/src/main/scala/com/mmold/bootstrap/interpreters/ServerBootstrapInterpreter.scala
@@ -6,7 +6,10 @@ import com.mmold.bootstrap.ServerBootstrapAlgebra
 
 /**
  * Application bootstrap.
+ * Bootstrapper that passes the server to application endpoints/controllers that can update the stats.
+ * @see Example: [[https://github.com/yorktsai/finch-example/blob/master/src/main/scala/tw/york/finch/example/Main.scala]].
  *
  * @param server Application bootstrap interpreter
  */
 final class ServerBootstrapInterpreter(val server: APIServer) extends ServerBootstrapAlgebra[IO]
+
