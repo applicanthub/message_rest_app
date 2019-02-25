465de0bb376a5c4668ec43fa88a6d1396cf2c7c5
Nick Odumo <nodumo@kivuto.com>
2019-02-25 15:03:11 -0500

    Update structure.

--- app_server/src/main/scala/com/mmold/SLF4JLogging.scala
+++ app_server/src/main/scala/com/mmold/SLF4JLogging.scala
@@ -5,10 +5,12 @@ import java.util.logging.Level
 import java.util.logging.LogManager
 import org.slf4j.bridge.SLF4JBridgeHandler
 
+/**
+ * Turn off Java util logging so that slf4j can configure it.
+ */
 trait SLF4JLogging { self: App =>
 
   init {
-    // Turn off Java util logging so that slf4j can configure it
     LogManager.getLogManager.getLogger("").getHandlers.toList.foreach { logger =>
       logger.setLevel(Level.OFF)
     }
