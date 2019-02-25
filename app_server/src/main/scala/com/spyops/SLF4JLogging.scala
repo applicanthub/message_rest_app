d52885cc36095e4c80c339131c83b6e010d25d11
applicanthub <47771075+applicanthub@users.noreply.github.com>
2019-02-25 09:49:25 -0500

    Update SLF4JLogging.scala

--- app_server/src/main/scala/com/spyops/SLF4JLogging.scala
+++ app_server/src/main/scala/com/spyops/SLF4JLogging.scala
@@ -6,6 +6,7 @@ import java.util.logging.LogManager
 import org.slf4j.bridge.SLF4JBridgeHandler
 
 trait SLF4JLogging { self: App =>
+  
   init {
     // Turn off Java util logging so that slf4j can configure it
     LogManager.getLogManager.getLogger("").getHandlers.toList.foreach { l =>
@@ -17,4 +18,5 @@ trait SLF4JLogging { self: App =>
   onExit {
     SLF4JBridgeHandler.uninstall()
   }
+  
 }
