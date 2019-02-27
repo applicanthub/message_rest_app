f743f843fa837dbeeea4c9d0b886f6b6e082b34d
applicanthub <47771075+applicanthub@users.noreply.github.com>
2019-02-27 10:44:58 -0500

    Update Command.scala

--- app_server/src/main/scala/com/mmold/console/Command.scala
+++ app_server/src/main/scala/com/mmold/console/Command.scala
@@ -10,18 +10,21 @@ trait Command {
   /**
    * The console command name.
    *
+   * @author Nick Odumo Feb 2019
    */
   val name: String
 
   /**
    * The console command description.
    *
+   * @author Nick Odumo Feb 2019
    */
   val description: String
 
   /**
    * Execute the console command.
    *
+   * @author Nick Odumo Feb 2019
    * @param args Arguments as string
    */
   def handle(args: String): Unit
