35429679b50b7b8629179ae2fa48de612f017866
Messgenger <qlikstuff@mail.com>
2019-02-28 19:25:33 -0500

    Minor changes to architecture to make the project more profesional.

--- app_server/src/main/scala/com/mmold/business/domain/building/models/values/Flooring.scala
+++ app_server/src/main/scala/com/mmold/business/domain/building/models/values/Flooring.scala
@@ -10,4 +10,22 @@ case object Laminate extends Flooring
 
 case object Wood extends Flooring
 
-case object Other extends Flooring
+final case class Other(name: String) extends Flooring
+
+object Flooring {
+
+  /**
+   * Parse from string.
+   *
+   * @author Nick Odumo Feb 2019
+   * @param name Name of the value
+   */
+  def fromString(name: String): Option[Flooring] = name.toLowerCase match {
+    case "carpet" => Some(Carpet)
+    case "concrete" => Some(Concrete)
+    case "laminate" => Some(Laminate)
+    case "wood" => Some(Wood)
+    case _ => Some(Other(name))
+  }
+
+}
