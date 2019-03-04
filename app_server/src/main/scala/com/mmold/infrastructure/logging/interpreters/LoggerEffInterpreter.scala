e0ec4ca49b6f7af84e12c2d015248f85c0756c87
Messgenger <qlikstuff@mail.com>
2019-03-04 17:15:49 -0500

    Added support for template

---
+++ app_server/src/main/scala/com/mmold/infrastructure/logging/interpreters/LoggerEffInterpreter.scala
@@ -0,0 +1,36 @@
+package com.mmold.infrastructure.logging.interpreters
+
+import java.io.PrintWriter
+
+import com.mmold.infrastructure.logging.interpreters.LoggerEffInterpreter.{ LogMessage }
+import org.atnos.eff.RightFold
+
+/**
+ * Printer logger Eff side-effecting logger.
+ *
+ * @author Nick Odumo Feb 2019
+ * @param path Path to log message at
+ */
+final class LoggerEffInterpreter(path: LogMessage) extends RightFold[LogMessage, Unit] {
+
+  type S = PrintWriter
+
+  val init: S = new PrintWriter(path)
+
+  def fold(a: LogMessage, s: S): S = {
+    s.println(a); s
+  }
+
+  def finalize(s: S): Unit =
+    s.close
+
+}
+
+object LoggerEffInterpreter {
+
+  type LogMessage = String
+
+  def apply(path: LogMessage): LoggerEffInterpreter =
+    new LoggerEffInterpreter(path)
+
+}
