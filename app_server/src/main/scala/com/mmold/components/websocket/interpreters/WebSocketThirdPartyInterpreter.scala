dc5726cc8b9234c368a2643d510bae77ff2d7633
applicanthub <47771075+applicanthub@users.noreply.github.com>
2019-03-03 19:42:52 -0500

    Update WebSocketThirdPartyInterpreter.scala

--- app_server/src/main/scala/com/mmold/components/websocket/interpreters/WebSocketThirdPartyInterpreter.scala
+++ app_server/src/main/scala/com/mmold/components/websocket/interpreters/WebSocketThirdPartyInterpreter.scala
@@ -4,9 +4,21 @@ import cats.effect.IO
 import com.mmold.components.websocket.WebSocketThirdPartyAlgebra
 import WebSocketThirdPartyInterpreter.{ F, Collection, Channel, Event, Payload }
 
+/**
+ * Pusher websocket implementation.
+ *
+ * @author Nick Odumo Feb 2019
+ * @todo Upgrade class with a lot more functionality 
+ * @param pusherConfig  
+ */
 final class WebSocketThirdPartyInterpreter(
   pusherConfig: PusherConfig) extends WebSocketThirdPartyAlgebra[F, Collection, Channel, Event, Payload] {
-
+  
+  /**
+   * Trigger pusher message to relevant channels.
+   *
+   * @author Nick Odumo Feb 2019
+   */
   def trigger(
     channels: Collection[Channel],
     event: Event,
@@ -26,4 +38,4 @@ object WebSocketThirdPartyInterpreter {
 
   type Payload = PusherPayload
 
-}
+} 
