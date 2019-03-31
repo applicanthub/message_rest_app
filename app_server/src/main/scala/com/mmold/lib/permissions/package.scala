89a30c493007a6179aeabada63cccbc6a76bd670
Messgenger <qlikstuff@mail.com>
2019-03-30 20:38:41 -0400

    Added support for template

--- app_server/src/main/scala/com/mmold/lib/permissions/package.scala
+++ app_server/src/main/scala/com/mmold/lib/permissions/package.scala
@@ -6,7 +6,7 @@ package com.mmold.lib
  * @author Nick Odumo March 2019
  * @see
  * @see https://github.com/spatie/laravel-permission
- * @see
+ * @see https://github.com/JosephSilber/bouncer
  */
 package object permissions {
 
